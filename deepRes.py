75768a7199a0debc77caf13986c12a59fb8b1ad2
Jose M. de la Rosa <delarosatrevin@gmail.com>
2024-03-31 10:46:37 -0500

    Big-refactor, indexing problem

--- deepRes.py
+++ deepRes.py
@@ -41,195 +41,173 @@ import mrcfile
 # The method accepts as input a 3D crioEM map and the mask
 # both with sampling rate of 1 A/pixel for network 1 or 0.5 A/pixel for network 2
 
-boxDim = 13
-boxDim2 = boxDim//2
-maxSize = 1000
-
-def getBox(V,z,y,x):
-    boxDim2 = boxDim//2
-    box = V[z-boxDim2:z+boxDim2+1,y-boxDim2:y+boxDim2+1,x-boxDim2:x+boxDim2+1]
-    box = box/np.linalg.norm(box)
-    return box
-
-def getDataIfFname(fnameOrNumpy):
-  if isinstance(fnameOrNumpy, str):
-    raise Exception("Pass numpy data as input")
-  elif isinstance(fnameOrNumpy, np.ndarray):
-    return fnameOrNumpy
-  else:
-    raise ValueError("Error, input must be either file name or numpy array")
+VOL_TH = 0.00015
+MASK_TH = 0.15
 
 
 class VolumeManager(Sequence):
-    def __init__(self, fnVolOrNumpy, fnMaskOrNumpy):
-        self.V =  getDataIfFname(fnVolOrNumpy)
-        self.M =  getDataIfFname(fnMaskOrNumpy)
+    def __init__(self, volArray, maskArray, boxSize=13, maxSize=1000):
+        self.boxSize = boxSize
+        self.maxSize = maxSize
+        self.volArray = volArray
+        self.maskArray = maskArray
+        print(">>> DEBUG: ", f"volShape: {volArray.shape}")
+        print(">>> DEBUG: ", f"maskArray: {maskArray.shape}")
+        vx = 0
+        for z, y, x in self.iterBox():
+            if self.evalPos(z, y, x):
+                vx += 1
 
-        self.Zdim, self.Ydim, self.Xdim = self.V.shape
-        # print(self.Zdim, self.Ydim, self.Xdim)
-        #calculate total voxels (inside mask)
-        # FIXME: Refactor this loop to increase the readability
-        vx=0
-        for self.z in range(boxDim2,self.Zdim-boxDim2):
-           for self.y in range(boxDim2,self.Ydim-boxDim2):
-              for self.x in range(boxDim2,self.Xdim-boxDim2):
-                  if self.M[self.z,self.y,self.x]>0.15 and self.V[self.z,self.y,self.x]>0.00015:
-                        if ((self.x+self.y+self.z)%2)==0:
-                             vx=vx+1
-        #print vx
-        self.st=vx//maxSize
-        if vx % maxSize >0:
-           self.st=self.st+1
-        #print self.st
-
-
-        self.x = boxDim2
-        self.y = boxDim2
-        self.z = boxDim2
-
-
-        #print (self.x   ,   self.y  ,  self.z, self.M[self.z,self.y,self.x])
-        if self.M[self.z,self.y,self.x]<=0.15:
+        self.st = vx // self.maxSize + (vx % self.maxSize)
+        h = self.boxSize // 2
+        self.pos = np.asarray([h, h, h])
+        if self.maskArray[h, h, h] <= MASK_TH:
             self.advance()
 
+        print(">>> DEBUG: ", f"volShape: {self.volArray.shape}")
+        print(">>> DEBUG: ", f"maskArray: {self.maskArray.shape}")
 
     def __len__(self):
-        return maxSize
+        return self.maxSize
 
-    def getNumberOfBlocks(self):
+    @property
+    def numberOfBlocks(self):
         return self.st
 
     def advancePos(self):
-         self.x+=1
-         if self.x==self.Xdim-boxDim2:
-            self.x=boxDim2
-            self.y+=1
-            if self.y==self.Ydim-boxDim2:
-                self.x = boxDim2
-                self.y = boxDim2
-                self.z+=1
-                if self.z==self.Zdim-boxDim2:
-                    return False
-         return True
+        """ Return new the position when need to move, None otherwise. """
+        advance = True
+        zdim, ydim, xdim = self.volArray.shape
+        h = self.boxSize // 2
+        print(f">>> DEBUG: BEFORE \n\tpos = {self.pos}")
+        z, y, x = self.pos
+        x += 1
+        if x == xdim - h:
+            x = h
+            y += 1
+            if y == ydim - h:
+                y = h
+                z += 1
+                if z == zdim - h:
+                    advance = False
+        self.pos[:] = z, y, x
+        print(f">>> DEBUG: AFTER \n\tpos = {self.pos}, advance: {advance}")
+        return advance
 
     def advance(self):
-        ok=self.advancePos()
+        ok = self.advancePos()
         while ok:
-            if self.M[self.z,self.y,self.x]>0.15 and self.V[self.z,self.y,self.x]>0.00015:
-                    if (self.x+self.y+self.z)%2==0:
-                        break
-            ok=self.advancePos()
-        return ok 
+            z, y, x = self.pos
+            if self.evalPos(z, y, x):
+                break
+            ok = self.advancePos()
+        return ok
 
-    def __getitem__(self,idx):
+    def evalPos(self, z, y, x, rest=0):
+        M = self.maskArray
+        V = self.volArray
+        try:
+            v = M[z, y, x] > MASK_TH and V[z, y, x] > VOL_TH and (z + y + x) % 2 == rest
+        except Exception as e:
+            print(f">>> DEBUG: EXCEPTION ON INDEX {(z, y, x)}")
+            raise e
+        return v
+
+    def getBox(self):
+        """ Get a box (with selected boxSize) at the given coordinates. """
+        z, y, x = self.pos
+        h1 = self.boxSize // 2
+        h2 = self.boxSize - h1
+        box = self.volArray[z - h1:z + h2, y - h1:y + h2, x - h1:x + h2]
+        box = box / np.linalg.norm(box)
+        return box
+
+    def __getitem__(self, idx):
         count = 0
         batchX = []
         ok = True 
-        while count < maxSize and ok:
-            batchX.append(getBox(self.V, self.z, self.y, self.x))
+        while count < self.maxSize and ok:
+            batchX.append(self.getBox())
+            print(">>>> --------  idx", idx)
             ok = self.advance()
             count += 1
         batchX = np.asarray(batchX).astype("float32")
         batchX = batchX.reshape(count, batchX.shape[1], batchX.shape[2], batchX.shape[3], 1)
         return batchX
-   
 
+    def iterBox(self, offset=0):
+        """ Iterate z, y, x over box boundaries. """
+        h = self.boxSize // 2
+        zdim, ydim, xdim = self.volArray.shape
+        for z in range(h + offset, zdim - h):
+            for y in range(h + offset, ydim - h):
+                for x in range(h + offset, xdim - h):
+                    yield z, y, x
 
-def produceOutput(fnVolInOrNumpy, fnMaskOrNumpy, model, sampling, Y, fnVolOut):
-    V = getDataIfFname(fnVolInOrNumpy)
-    Orig = V
-    M = getDataIfFname(fnMaskOrNumpy)
-    V = V*0
-    Zdim, Ydim, Xdim = V.shape
-    idx = 0
+    def getOutput(self, modelNumber, sampling, Y):
+        V = self.volArray
+        OV = V * 0
 
-    if model==1:
-       if 2*sampling > 2.5:
-          minValue=2*sampling
-       else:
-          minValue=2.5
-    if model==2:
-       if 2*sampling > 1.5:
-          minValue=2*sampling
-       else:
-          minValue=1.5        
+        samplingTh = 2.5 if modelNumber == 1 else 1.5
+        minValue = max(sampling * 2, samplingTh)
+        maxValue = 12.9 if modelNumber == 1 else 5.9
 
-    # FIXME: Refactor these loops to increase the readability
-    boxDim2 = boxDim//2
-    for z in range(boxDim2,Zdim-boxDim2):
-        for y in range(boxDim2,Ydim-boxDim2):
-            for x in range(boxDim2,Xdim-boxDim2):
-                if M[z,y,x]>0.15 and Orig[z,y,x]>0.00015:
-                    if ((x+y+z)%2)==0:
+        idx = 0
+        for z, y, x in self.iterBox():
+            if self.evalPos(z, y, x):
+                OV[z, y, x] = max(min(Y[idx], maxValue), minValue)
+                idx += 1
 
-                        if model==1:
-                           if Y[idx]>12.9:
-                              Y[idx]=12.9
-                           if Y[idx]<minValue:
-                              Y[idx]=minValue
-                        if model==2:
-                           if Y[idx]>5.9:
-                              Y[idx]=5.9
-                           if Y[idx]<minValue:
-                              Y[idx]=minValue
-
-                        V[z,y,x]=Y[idx]
-                        idx=idx+1
-
-    ###mean
-    for z in range(boxDim2+1,Zdim-boxDim2):
-        for y in range(boxDim2+1,Ydim-boxDim2):
-            for x in range(boxDim2+1,Xdim-boxDim2):
-                if M[z,y,x]>0.15 and Orig[z,y,x]>0.00015:
-                    if ((x+y+z)%2)!=0:
-                       col=0
-                       ct=0
-                       if V[z+1,y,x]>0:
-                          col+=V[z+1,y,x]
-                          ct+=1
-                       if V[z-1,y,x]>0:
-                          col+=V[z-1,y,x]
-                          ct+=1
-                       if V[z,y+1,x]>0:
-                          col+=V[z,y+1,x]
-                          ct+=1
-                       if V[z,y-1,x]>0:
-                          col+=V[z,y-1,x]
-                          ct+=1
-                       if V[z,y,x+1]>0:
-                          col+=V[z,y,x+1]
-                          ct+=1
-                       if V[z,y,x-1]>0:
-                          col+=V[z,y,x-1]
-                          ct+=1
-                       if ct==0:
-                          V[z,y,x]=0
-                          ct=1
-                       else:
-                          meansum=col/ct
-                          V[z,y,x]=meansum         
-
-    if fnVolOut is not None:
-        with mrcfile.open(fnVolOut, mode='w+') as mrc:
-            mrc.set_data(V)
+        for z, y, x in self.iterBox(offset=1):
+            if self.evalPos(z, y, x, rest=1):
+                col = 0
+                ct = 0
+                if OV[z + 1, y, x] > 0:
+                    col += OV[z + 1, y, x]
+                    ct += 1
+                if OV[z - 1, y, x] > 0:
+                    col += OV[z - 1, y, x]
+                    ct += 1
+                if OV[z, y + 1, x] > 0:
+                    col += OV[z, y + 1, x]
+                    ct += 1
+                if OV[z, y - 1, x] > 0:
+                    col += OV[z, y - 1, x]
+                    ct += 1
+                if OV[z, y, x + 1] > 0:
+                    col += OV[z, y, x + 1]
+                    ct += 1
+                if OV[z, y, x - 1] > 0:
+                    col += OV[z, y, x - 1]
+                    ct += 1
+                if ct == 0:
+                    OV[z, y, x] = 0
+                    ct = 1
+                else:
+                    meansum = col / ct
+                    OV[z, y, x] = meansum
+        return OV
 
 
 def main(fnModel, fnVolIn, fnMask, sampling, fnVolOut):
-    model = load_model(fnModel)
-
-    mrcVol = mrcfile.open(fnVolIn)
-    mrcMask = mrcfile.open(fnMask)
-
-    manager = VolumeManager(mrcVol.data, mrcMask.data)
-    Y = model.predict_generator(manager, manager.getNumberOfBlocks())
-
     modelName = os.path.basename(fnModel)
     if modelName.endswith("model_w13.h5"):
-        model = 1
+        modelNumber = 1
     elif modelName.endswith("model_w7.h5"):
-        model = 2
+        modelNumber = 2
+    else:
+        raise Exception("Unknown model {modelName}, it should be either"
+                        "model_w13.h5 or model_w7.h5")
+    model = load_model(fnModel)
+    mrcVol = mrcfile.open(fnVolIn)
+    mrcMask = mrcfile.open(fnMask)
+    manager = VolumeManager(mrcVol.data, mrcMask.data)
+    Y = model.predict_generator(manager, manager.numberOfBlocks)
+    outputArray = manager.getOutput(modelNumber, sampling, Y)
 
-    produceOutput(mrcVol.data, mrcMask.data, model, sampling, Y, fnVolOut)
+    # Write output volume
+    with mrcfile.open(fnVolOut, mode='w+') as mrc:
+        mrc.set_data(outputArray)
 
 
 if __name__ == "__main__":
@@ -246,12 +224,6 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output",
                         help="output resolution map", required=True)
     args = parser.parse_args()
-
-    fnModel = args.dl_model
-    fnVolIn = args.map 
-    fnMask = args.mask
-    sampling = float(args.sampling)
-    fnVolOut = args.output
 
     main(args.dl_model, args.map, args.mask, args.sampling, args.output)
 
